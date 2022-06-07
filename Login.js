5bcb3e4c16f3f85ffc02fbdbec0eb9dcbd6611eb
Spencer Ehardt <spencerehardt@gmail.com>
2022-06-07 13:43:31 -0600

    June 7

---
+++ Login.js
@@ -0,0 +1,36 @@
+import {useState} from "react";
+import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+
+const Login = () => {
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [oneTimePassword, setOneTimePassword] = useState(null);
+
+  return (
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={setPhoneNumber}
+        value={phoneNumber}
+        placeholder="801-555-1212"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setOneTimePassword}
+        value={oneTimePassword}
+        placeholder="1234"
+        keyboardType="numeric"
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
+
+export default Login;
